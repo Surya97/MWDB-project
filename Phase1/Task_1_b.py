dda66aa24e9aa972afc73d60025418a137087050
Surya97 <suryavamsi8@gmail.com>
2019-09-07 20:34:16 -0700

    Added functionality to get LBP features for all images in a given folder

--- Phase1/Task_1_b.py
+++ Phase1/Task_1_b.py
@@ -1,12 +1,44 @@
 from Phase1.LBP import LocalBinaryPatterns
+from pathlib import Path
 import misc
+import os
+from tqdm import tqdm
+import progressbar
+from time import sleep
+import numpy
 
+lbp = LocalBinaryPatterns(100, 1)
+folder = os.path.join(Path(os.path.dirname(__file__)).parent, "data/Hands")
+files_in_directory = misc.get_images_in_directory(folder)
 
-lbp = LocalBinaryPatterns(24, 8)
+# bar = progressbar.ProgressBar(maxval=len(files_in_directory.items()),
+#                               widgets=[progressbar.Bar('=', '[', ']'), ' ', progressbar.Percentage()])
 
-image = misc.read_image("data/Hands/Hand_0000002.jpg", gray=True)
-misc.plot_image(image)
-image = misc.convert2gray(image)
-lbp_image = lbp.computeLBP(image)
-misc.plot_image(lbp_image)
+dict_file_pattern = {}
+count = 0
+print('Files in the directory', len(files_in_directory.items()))
+lbp_patterns = []
+# bar.start()
+for file, path in tqdm(files_in_directory.items()):
+    result = []
+    try:
+        image = misc.read_image(path)
+        image_gray = misc.convert2gray(image)
+        windows = misc.split_into_windows(image_gray, 100, 100)
+        # print(windows)
+        for window in windows:
+            lbp_pattern = lbp.computeLBP(window)
+            # print(lbp_pattern)
+            result.append(lbp_pattern)
+
+        # print('Result shape', len(result), len(result[0]))
+        dict_file_pattern[file] = result
+        lbp_patterns.append(result)
+    except:
+        lbp_patterns.append(result)
+
+# bar.finish()
+print(len(list(files_in_directory.keys())), len(lbp_patterns))
+misc.save2csv(list(files_in_directory.keys()), lbp_patterns, os.path.dirname(__file__), feature='LBP')
+
 
