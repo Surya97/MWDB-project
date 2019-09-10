6e4c97c90b4b69e43a44046a6dd67ab80d4247e7
Surya97 <suryavamsi8@gmail.com>
2019-09-09 20:57:31 -0700

    Added normalization for LBP histogram

--- Phase1/LBP.py
+++ Phase1/LBP.py
@@ -6,6 +6,16 @@ class LocalBinaryPatterns:
         self.numPoints = numPoints
         self.radius = radius
 
-    def computeLBP(self, image):
+    def computeLBP(self, image, eps=1e-7):
         lbp = feature.local_binary_pattern(image=image, P=self.numPoints, R=self.radius, method='uniform')
-        return lbp
+        (hist, _) = np.histogram(lbp.ravel(),
+                                 bins=100,
+                                 range=(0.0, 255.0))
+
+        hist = hist.astype("float")
+        hist /= (hist.sum() + eps)
+
+        # print('Len hist to list', len(hist.tolist()))
+        # return the histogram of Local Binary Patterns
+        return hist.tolist()
+
