e9b32a6266aff004b92487c4b2629ba1a47ba799
Suryavamsi Tenneti <suryavamsi8@gmail.com>
2019-09-13 20:58:14 -0700

    LBP similarity task sample

--- Phase1/LBP_similarity.py
+++ Phase1/LBP_similarity.py
@@ -1,36 +1,53 @@
-def ranking(ar):
-    n = len(ar)
-    rank_vector = [0]*n
-    for i in range(n):
-        l = 1
-        m = 1
-        for j in range(i):
-            if ar[j] < ar[i]:
-                l += 1
-            if ar[j] == ar[i]:
-                m += 1
+import spearman_similarity
+from LBP import LocalBinaryPatterns
+import misc
+from pathlib import Path
+import os
+import collections
+from itertools import islice
+from tqdm import tqdm
 
-        for j in range(i+1, n):
-            if ar[j] < ar[i]:
-                l += 1
-            if ar[j] == ar[i]:
-                m += 1
+image = 'Hand_0008663.jpg'
+folder = os.path.join(Path(os.path.dirname(__file__)).parent, "data/Test dataset")
 
-        rank_vector[i] = l + (m-1) * 0.5
+image_input = misc.read_image(os.path.join(folder, image))
+misc.plot_image(image_input)
 
-    return rank_vector
+image_input_gray = misc.convert2gray(image_input)
+
+lbp = LocalBinaryPatterns(4, 1)
+
+input_image_lbp = []
+
+input_image_windows = misc.split_into_windows(image_input_gray, 100, 100)
+for input_image_window in input_image_windows:
+    lbp_pattern = lbp.computeLBP(input_image_window)
+    if len(input_image_lbp) == 0:
+        input_image_lbp = lbp_pattern
+    else:
+        input_image_lbp += lbp_pattern
 
 
-def correlation_coefficient(x, y):
-    n = len(x)
-    sum_x = sum(x)
-    sum_y = sum(y)
-    sum_xy = sum([a*b for a,b in zip(x,y)])
-    sum_square_x = sum([a*a for a in x])
-    sum_square_y = sum([a*a for a in y])
+dataset_lbp_features = misc.load_from_pickle(os.path.dirname(__file__), 'LBP')
+print('lbp_features_dataset length', len(dataset_lbp_features.items()))
+input_image_feature_rank = spearman_similarity.ranking(input_image_lbp)
+print('input_feature_rank', input_image_feature_rank)
+ranking = {}
 
-    correlation = (n * sum_xy - sum_x * sum_y) / pow((n * sum_square_x - sum_x * sum_x) *
-                                                     (n * sum_square_y - sum_y * sum_y), 0.5)
+for image_id, feature in dataset_lbp_features.items():
+    image_feature_rank = spearman_similarity.ranking(feature)
+    # print(image_feature_rank, input_image_feature_rank)
+    correlation = spearman_similarity.correlation_coefficient(image_feature_rank, input_image_feature_rank)
+    ranking[image_id] = correlation
 
-    return correlation
+sorted_results = collections.OrderedDict(sorted(ranking.items(), key=lambda val: val[1], reverse=True))
+
+k = 5
+
+top_k_items = {k: sorted_results[k] for k in list(sorted_results)[:k]}
+
+for image_id in top_k_items.keys():
+    misc.plot_similar_images(image_id, top_k_items[image_id])
+
+
 
