c9cd31b7adcc150a43829bba53436b46b5041896
Suryavamsi Tenneti <suryavamsi8@gmail.com>
2019-09-14 04:37:20 -0700

    Added code comments

--- Phase1/features_images.py
+++ Phase1/features_images.py
@@ -5,13 +5,24 @@ from tqdm import tqdm
 import LBP
 import HOG
 
+'''
+A utility function to print the array in a more elegant manner
+'''
+
 
 def print_array(feature_vector):
     print('\n'.join([''.join(['{:4}'.format(item) for item in row])
                      for row in feature_vector]))
 
+
+'''
+Class for handling all the feature vector generation for both single and multiple images
+'''
+
+
 class FeaturesImages:
 
+    # Initialize the class variables model name, folder path and model
     def __init__(self, model_name, folder_path):
         self.model_name = model_name
         self.folder_path = folder_path
@@ -22,6 +33,13 @@ class FeaturesImages:
             self.split_windows = True
         elif self.model_name == 'HOG':
             self.model = HOG.Hog(orientations=9, pixels_per_cell=(8, 8), cells_per_block=(2, 2))
+
+    '''
+    For the folder path specified, the below function computes the feature vectors
+    and stores them in a pickle file based on the respective feature extraction model using
+    a helper function 'compute_image_features'. Use of the package 'tqdm' shows a 
+    smart progress meter.
+    '''
 
     def compute_features_images_folder(self):
         if self.model is None:
@@ -40,6 +58,12 @@ class FeaturesImages:
                 folder_images_features_dict[images[i]] = features_image_folder[i]
 
             misc.save2pickle(folder_images_features_dict, os.path.dirname(__file__), feature=self.model_name)
+
+    '''
+    Given an image path, based on the model requirements the features vectors are retrieved and
+    based on the attribute value for 'print_arr' the function either prints the feature vector 
+    or returns the feature vector.
+    '''
 
     def compute_image_features(self, image, print_arr=False):
         try:
@@ -61,4 +85,5 @@ class FeaturesImages:
         finally:
             if not print_arr:
                 return image_feature
-
+            else:
+                print_array(image_feature)
