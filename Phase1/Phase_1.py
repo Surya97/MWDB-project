a76ccc4d4d06cef07cceff49331bf849b3a3b693
Suryavamsi Tenneti <suryavamsi8@gmail.com>
2019-09-14 04:38:38 -0700

    Issue fix: Class name change not applied

--- Phase1/Phase_1.py
+++ Phase1/Phase_1.py
@@ -1,5 +1,5 @@
 import sys
-from features_images_folder import FeaturesFolder
+from features_images import FeaturesImages
 
 arguments = sys.argv[1:]
 
@@ -9,9 +9,10 @@ model = arguments[1]
 
 if task == '1':
     image_id = arguments[2]
+
 elif task == '2':
     folder_path = arguments[2]
-    features_folder = FeaturesFolder(model, folder_path)
+    features_folder = FeaturesImages(model, folder_path)
     features_folder.compute_features_images_folder()
 if task == '3':
     image_id = arguments[2]
