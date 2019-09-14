b6368c3e6c0fe3b19b4ac9ba04ed84b28b2de458
Suryavamsi Tenneti <suryavamsi8@gmail.com>
2019-09-13 20:59:16 -0700

    modified code to store feature vectors as a pickle file

--- Phase1/Task_1_b.py
+++ Phase1/Task_1_b.py
@@ -3,38 +3,27 @@ from pathlib import Path
 import misc
 import os
 from tqdm import tqdm
-import progressbar
-from time import sleep
-import numpy
 
-lbp = LocalBinaryPatterns(8, 2)
-folder = os.path.join(Path(os.path.dirname(__file__)).parent, "data/Hands")
+
+lbp = LocalBinaryPatterns(24, 3)
+folder = os.path.join(Path(os.path.dirname(__file__)).parent, "data/Test dataset")
 files_in_directory = misc.get_images_in_directory(folder)
-
-# bar = progressbar.ProgressBar(maxval=len(files_in_directory.items()),
-#                               widgets=[progressbar.Bar('=', '[', ']'), ' ', progressbar.Percentage()])
-
 dict_file_pattern = {}
 count = 0
 print('Files in the directory', len(files_in_directory.items()))
 lbp_patterns = []
-# bar.start()
 for file, path in tqdm(files_in_directory.items()):
     result = []
     try:
         image = misc.read_image(path)
         image_gray = misc.convert2gray(image)
         windows = misc.split_into_windows(image_gray, 100, 100)
-        # print(windows)
         for window in windows:
             lbp_pattern = lbp.computeLBP(window)
-            # print(lbp_pattern)
             if len(result) == 0:
                 result = lbp_pattern
             else:
                 result += lbp_pattern
-
-        # print('Result shape', len(result), len(result[0]))
         dict_file_pattern[file] = result
         lbp_patterns.append(result)
     except:
@@ -42,6 +31,11 @@ for file, path in tqdm(files_in_directory.items()):
 
 # bar.finish()
 print(len(list(files_in_directory.keys())), len(lbp_patterns))
+images = list(files_in_directory.keys())
+folder_features_dict = {}
+for i in range(len(images)):
+    folder_features_dict[images[i]] = lbp_patterns[i]
+
 # misc.save2csv(zip(list(files_in_directory.keys()), lbp_patterns), os.path.dirname(__file__), feature='LBP')
-misc.save2pickle(zip(list(files_in_directory.keys()), lbp_patterns), os.path.dirname(__file__), feature='LBP')
+misc.save2pickle(folder_features_dict, os.path.dirname(__file__), feature='LBP')
 
