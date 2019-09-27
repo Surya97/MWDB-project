6d102bd7cbcb2f5eb6eb768b510a96855e6a6c92
Suryavamsi Tenneti <suryavamsi8@gmail.com>
2019-09-27 04:52:19 -0700

    Refactor: Similar Images class, all the similar images code is removed from individual classes and placed in a single function here.

--- Phase1/similar_images.py
+++ Phase1/similar_images.py
@@ -1,10 +1,15 @@
 from features_images import FeaturesImages
 import os
 from pathlib import Path
+import misc
+from tqdm import tqdm
+import collections
 
 '''
-Similarity measure base class. Calls the similarity function of the particular model. 
+Similarity measure base class. 'get_similar_images' function plots the top k images based on the 
+similarity measure suitable for the model.
 '''
+
 
 class Similarity:
     def __init__(self, model_name, test_image_id, k):
@@ -18,4 +23,20 @@ class Similarity:
         test_image_path = os.path.join(test_folder_path, self.test_image_id)
         test_image_features = features_images.compute_image_features(test_image_path)
         model = features_images.get_model()
-        model.get_similar_images(test_image_features, self.k, test_folder_path, self.test_image_id)
+        dataset_images_features = misc.load_from_pickle(os.path.dirname(__file__), self.model_name)
+        ranking = {}
+        for image_id, feature_vector in tqdm(dataset_images_features.items()):
+            distance = model.similarity_fn(test_image_features, feature_vector)
+            ranking[image_id] = distance
+
+        sorted_results = collections.OrderedDict(sorted(ranking.items(), key=lambda val: val[1],
+                                                        reverse=model.reverse_sort))
+        top_k_items = {item: sorted_results[item] for item in list(sorted_results)[:self.k + 1]}
+
+        plot_images = {}
+        for image_id in top_k_items.keys():
+            if image_id != self.test_image_id:
+                image_path = os.path.join(test_folder_path, image_id)
+                plot_images[image_path] = top_k_items[image_id]
+
+        misc.plot_similar_images(plot_images)
