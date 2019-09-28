5014e3ae5a7b96fa8e2440c19bb93ce760bfec8b
santhosh1bijinemula <santhoshprogramming@gmail.com>
2019-09-28 13:45:47 -0700

    test3

--- Phase2/SVD.py
+++ Phase2/SVD.py
@@ -16,6 +16,7 @@ class SVD:
         # print(len(self.u), len(self.u[0]))
         # print(len(self.s))
         # print(len(self.vh), len(self.vh[0]))
+
         self.decomposed_database_matrix = np.matmul(np.matmul(self.u, np.diag(self.s)), self.vh)
         #print(len(self.decomposed_database_matrix), len(self.decomposed_database_matrix[0]))
         return self.print_term_weight_pairs()
