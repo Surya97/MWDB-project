5787f8fcb27fd07068672616541df3fd7e1f2af9
Suryavamsi Tenneti <suryavamsi8@gmail.com>
2019-09-28 12:07:29 -0700

    Refactor: SVD get_eigen_values changed to get_feature_weight_values

--- Phase2/SVD.py
+++ Phase2/SVD.py
@@ -17,12 +17,13 @@ class SVD:
         # print(len(self.s))
         # print(len(self.vh), len(self.vh[0]))
         self.decomposed_database_matrix = np.matmul(np.matmul(self.u, np.diag(self.s)), self.vh)
+        print(len(self.decomposed_database_matrix), len(self.decomposed_database_matrix[0]))
         return self.print_term_weight_pairs()
 
     def get_eigen_vectors(self):
         return self.vh[:self.k_components]
 
-    def get_eigen_values(self):
+    def get_feature_weight_values(self):
         return self.s[:self.k_components]
 
     def print_term_weight_pairs(self):
