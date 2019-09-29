1f0f96234c39a4625c88f006b052fef73e2a5329
Suryavamsi Tenneti <suryavamsi8@gmail.com>
2019-09-28 20:18:29 -0700

    Implementation:  Added functionality to save the reduced dimension vector to pickle file

--- Phase2/Decomposition.py
+++ Phase2/Decomposition.py
@@ -18,6 +18,8 @@ class Decomposition:
         self.feature_extraction_object = FeaturesImages(self.feature_extraction_model_name, self.test_folder_path)
         self.feature_extraction_model = self.feature_extraction_object.get_model()
         self.database_matrix = []
+        self.database_image_id = []
+        self.reduced_pickle_file_folder = os.path.join(os.path.dirname(__file__), 'pickle_files')
         self.set_database_matrix()
 
     def set_database_matrix(self):
@@ -28,6 +30,7 @@ class Decomposition:
         database_images_features = misc.load_from_pickle(pickle_file_directory, self.feature_extraction_model_name)
         for image_id, feature_vector in database_images_features.items():
             self.database_matrix.append(feature_vector)
+            self.database_image_id.append(image_id)
 
     def dimensionality_reduction(self):
         if self.decomposition_name == 'PCA':
@@ -36,4 +39,12 @@ class Decomposition:
             self.decomposition_model = SVD(self.database_matrix, self.k_components)
 
         self.decomposition_model.decompose()
+        decomposed_database_matrix = self.decomposition_model.get_decomposed_data_matrix()
+        reduced_dimension_folder_images_dict = {}
+        for image_id, reduced_feature_vector in zip(self.database_image_id, decomposed_database_matrix):
+            reduced_dimension_folder_images_dict[image_id] = reduced_feature_vector
+
+        misc.save2pickle(reduced_dimension_folder_images_dict, self.reduced_pickle_file_folder,
+                         feature=self.feature_extraction_model_name)
+
 
