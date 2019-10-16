b5f983daa01286d7cb72501521ff3cbe58f03a92
Suryavamsi Tenneti <suryavamsi8@gmail.com>
2019-10-16 13:05:32 -0700

    Task 4 implemented

--- Phase1/misc.py
+++ Phase1/misc.py
@@ -67,8 +67,8 @@ def save2pickle(tuples, path, feature):
     outfile.close()
 
 
-def load_from_pickle(path, feature,k=-1):
-    final_path = os.path.join(path, feature+'.pkl')
+def load_from_pickle(path, feature=None, k=-1):
+    final_path = os.path.join(path, (feature or '') + '.pkl')
     print('loading from pickle file path', final_path)
     infile = open(final_path, 'rb')
     dataset_features = pickle.load(infile)
