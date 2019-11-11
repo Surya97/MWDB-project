c62b056240052800ac2e4135e60df94029929164
santhosh1bijinemula <santhoshprogramming@gmail.com>
2019-11-10 17:05:20 -0700

    Standalone Directed Gprah

---
+++ Phase3/DirectedGraph.py
@@ -0,0 +1,76 @@
+
+
+class DirectedGraph(object):
+
+    def __init__(self, graph_dict=None):
+        if graph_dict == None:
+            graph_dict = {}
+        self.__graph_dict = graph_dict
+
+    def vertices(self):
+        return list(self.__graph_dict.keys())
+
+    def edges(self):
+        return self.__generate_edges()
+
+    def add_vertex(self, vertex):
+        if vertex not in self.__graph_dict:
+            self.__graph_dict[vertex] = []
+
+    def add_edge(self, edge):
+        (vertex1, vertex2) = tuple(edge)
+        if vertex1 in self.__graph_dict:
+            self.__graph_dict[vertex1].append(vertex2)
+        else:
+            self.__graph_dict[vertex1] = [vertex2]
+
+    def __generate_edges(self):
+        edges = []
+        for vertex in self.__graph_dict.keys():
+            for neighbour in self.__graph_dict[vertex]:
+                edges.append([vertex, neighbour])
+        return edges
+
+    def __str__(self):
+        res = "vertices: "
+        for k in self.__graph_dict:
+            res += str(k) + " "
+        res += "\nedges: "
+        for edge in self.__generate_edges():
+            res += str(edge) + " "
+        return res
+
+if __name__ == "__main__":
+
+    g = { "0" : ["1"],
+          "1" : ["2"],
+          "2" : ["1", "2", "3", "4"],
+          "3" : ["0", "2"],
+          "4" : ["2"],
+          "5" : []
+        }
+
+    graph = DirectedGraph(g)
+    print("Vertices of graph:")
+    print(graph.vertices())
+
+    print("Edges of graph:")
+    print(graph.edges())
+
+    print("Add an edge:")
+    graph.add_edge({"5", "6"})
+
+    print("Vertices of graph:")
+    print(graph.vertices())
+
+    print("Edges of graph:")
+    print(graph.edges())
+
+    print('Adding an edge {"4","7"} with new vertices:')
+    graph.add_edge({"4", "7"})
+    print("Vertices of graph:")
+    print(graph.vertices())
+    print("Edges of graph:")
+    print(graph.edges())
+
+    print("Graph: ", graph.__str__())
