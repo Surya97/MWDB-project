46d0699a644d6cb32f389af9ca5ebad8d61145d5
Suryavamsi Tenneti <suryavamsi8@gmail.com>
2019-11-17 14:05:00 -0700

    Modified CLI to use label_features.py for feature extraction and also inputs modified

--- Phase3/Phase_3.py
+++ Phase3/Phase_3.py
@@ -1,27 +1,32 @@
-# Phase 3 CLI
-
 from KMeans import KMeans
-task = input("Please specify the task number: ")
-import os
-from pathlib import Path
-import misc
 from label_features import LabelFeatures
 
+
+task = input("Please specify the task number: ")
+
+
 if task == '2':
-    c = int(input("Enter the Number Of Clusters:"))
-    labeled_dataset_path = 'Data/testdata'
-    unlabeled_dataset_path = 'Data/testdata'
-    labelfeatures=LabelFeatures(labeled_dataset_path=labeled_dataset_path,unlabeled_dataset_path=unlabeled_dataset_path )
-    labelfeatures.set_features()
-    #x = KMeans(5, labeled_dataset_path=labelled_dataset_path, unlabeled_dataset_path=unlabelled_dataset_path )
-    print(len(labelfeatures.get_unlabeled_dataset_features().items()))
-    #x.fit(x.get_label_features('palmar'))
-    #similarity_val2 = x.get_similarity_val(unlabelled_dataset_path)
+    c = int(input("Enter the number Of clusters:"))
+    labelled_dataset_path = input('Enter labelled dataset path: ')
+    unlabelled_dataset_path = input('Enter unlabelled dataset path: ')
+    kmeans = KMeans(c)
+    label_features = LabelFeatures(labelled_dataset_path=labelled_dataset_path,
+                                   unlabelled_dataset_path=unlabelled_dataset_path)
+    label_features.set_features()
+    dorsal_features = label_features.get_label_features('dorsal')
+    palmar_features = label_features.get_label_features('palmar')
+    unlabelled_features = label_features.get_unlabeled_images_decomposed_features()
 
+    kmeans.fit(dorsal_features)
+    similarity_val1 = kmeans.get_similarity_val(labelled_dataset_features=dorsal_features,
+                                                unlabeled_dataset_features=unlabelled_features)
 
+    kmeans.fit(palmar_features)
+    similarity_val2 = kmeans.get_similarity_val(labelled_dataset_features=palmar_features,
+                                                unlabeled_dataset_features=unlabelled_features)
 
 elif task == '3':
-    test_dataset_path = 'Data/testdata'
+    test_dataset_path = input("Enter test dataset path: ")
 
 
 
