5adfe45373e791e26f44f3cde2e22396553a3765
santhosh1bijinemula <santhoshprogramming@gmail.com>
2019-11-28 18:47:37 -0700

    task 6

--- Phase3/Phase_3.py
+++ Phase3/Phase_3.py
@@ -16,12 +16,7 @@ if task == '2':
     c = int(input("Enter the number Of clusters:"))
     labelled_dataset_path = input('Enter labelled dataset path: ')
     unlabelled_dataset_path = input('Enter unlabelled dataset path: ')
-    # labelled_metadatasetpath = input('Enter the Labelled metadataset path: ')
-    # unlabelled_metadataset_path = input('Enter the Unlabelled metadataset path')
     kmeans = KMeans(c)
-    # metadata = Metadata(metadatapath='Data/labelled_set1.csv')
-    # Unlabelledimagesdop_dict = metadata.getimagesdop_dict()
-
     label_features = LabelFeatures(labelled_dataset_path=labelled_dataset_path,
                                    unlabelled_dataset_path=unlabelled_dataset_path)
     label_features.set_features()
@@ -36,7 +31,8 @@ if task == '2':
     kmeans.fit(palmar_features)
     similarity_val2 = kmeans.get_similarity_val(labelled_dataset_features=palmar_features,
                                                 unlabelled_dataset_features=unlabelled_features)
-    result = {}
+
+    result={}
     for image_id in list(unlabelled_features.keys()):
         if similarity_val1[image_id] <= similarity_val2[image_id]:
             result[image_id]='dorsal'
@@ -86,7 +82,7 @@ elif task == '5':
     num_hashfunctions = int(input("Enter the number Of Hashes per layer:"))
     q_image_id = input("Enter The ImageId:")
     t = int(input('Enter the Value of t:'))
-    lsh = MyCustomLSH(number_of_hashes_per_layer=6, number_of_features=256, num_layers=5)
+    lsh = MyCustomLSH(number_of_hashes_per_layer =num_hashfunctions, number_of_features =256, num_layers=num_layers)
     final_path = '../Phase2/pickle_files/LBP_PCA_11k.pkl'
     print('loading from pickle file path', final_path)
     infile = open(final_path, 'rb')
@@ -106,7 +102,7 @@ elif task == '5':
 
     lsh.save_result(result)
 
-elif task == '6':
+elif task == '6' :
     r = int(input('Number Of Images you would like to label as Relevant:'))
     ir = int(input('Number of Images you would like to label as Irrelevant:'))
     final_path = '../Phase2/pickle_files/LBP_PCA_11k.pkl'
@@ -116,29 +112,38 @@ elif task == '6':
     feedback = Feedback()
     task5_result = feedback.task5_result
 
-    num_image = {}
+    num_image={}
     count = 1
     rorir_map = {}
     for image_id, val in task5_result.items():
-        num_image[count] = image_id
+        num_image[count]=image_id
         print(count, image_id)
         rorir_map[num_image[count]] = -1
-        count += 1
+        count=count+1
 
-    while r > 0:
+    while r>0:
         ind = int(input('Enter the Image Number to Label as Relevant:'))
-        r -= 1
-        rorir_map[num_image[ind]] = 1
-    while ir > 0:
+        r= r-1
+        rorir_map[num_image[ind]]=1
+    while ir>0:
         ind = int(input('Enter the Image Number to Label as Irrelevant:'))
-        ir -= 1
+        ir= ir-1
         rorir_map[num_image[ind]] = 0
 
     feedback.generate_input_data(rorir_map, dataset_features)
     classifier = input("1.SVM\n2.DT\n3.PPR\n4.Prob\nSelect Classifier: ")
-    result = {}
 
     if classifier == 'DT':
         decisiontree = DecisionTree()
-        decisiontree.dataset = feedback.dataset
+        decisiontree.dataset=feedback.dataset
         dt = decisiontree.build_tree(decisiontree.dataset, 10, 1)
+        for image_id, label in rorir_map.items():
+            feature = dataset_features[image_id]
+            feature = list(feature)
+            feature.append(None)
+            val = decisiontree.predict(dt, feature)
+            rorir_map[image_id]=val
+
+    for image_id,val in rorir_map.items():
+        print(image_id, val)
+
